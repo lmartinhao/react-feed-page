84e848a4d0d3f943d2b92c810f671e219613585e
lola martinhao <lola.martinhao@gmail.com>
2023-01-11 14:00:39 -0300

    using extends on components props

--- src/Components/Comment.tsx
+++ src/Components/Comment.tsx
@@ -23,7 +23,11 @@ export function Comment({ content, onDeleteComment }: CommentProps) {
 
   return (
     <div className={styles.comment}>
-      <Avatar hasBorder={false} src='https://github.com/lmartinhao.png' alt="" />
+      <Avatar
+        hasBorder={false}
+        src='https://github.com/lmartinhao.png'
+        alt=""
+      />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
